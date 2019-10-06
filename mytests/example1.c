0997eb4837a31e7ee998536eaa9a431e3461afdb
aleks_dimovski <aleks.dimovski@gmail.com>
2019-10-06 12:06:31 +0200

    update 3

--- mytests/example1.c
+++
@@ -1,8 +0,0 @@
-
-
-int main() {
-  int x=[0,199]; input:
-  while (x <= 100) 
-    x = x + 1;
-  assert(x>=101);
-}
--- mytests/example6.c
+++
@@ -1,9 +0,0 @@
-
-
-int main() {
-  int x=[0,9], y=[0,9]; input:
-  int s = x - y;
-  if (s>=2) y=y+1;
-  assert(y>2);
-  return 0;
-}
